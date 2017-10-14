36401192db5a43241036e5f837eea78ead219f93
Saneyee Sarkar <saneyeesarkar@gmail.com>
2017-10-14 01:41:08 -0700

    it updates the contactcreated cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,9 +9,27 @@ namespace AddressBook.Controllers
         [HttpGet("/")]
         public ActionResult Index()
         {
+            List<Contact> allContacts = Contact.GetAll();
+            return View(allContacts);
+        }
+        [HttpGet("/addacontact")]
+        public ActionResult AddContactForm()
+        {
             return View();
         }
-        
+        [HttpPost("/contact/new")]
+        public ActionResult ContactsAdded()
+        {
+            Contact newContact = new Contact(Request.Form["name"],Request.Form["phone"],Request.Form["address"]);
+            List<Contact> allTheContacts = Contact.GetAll();
+            return View("ContactCreated", allTheContacts);
+        }
+        [HttpGet("/contact/new")]
+        public ActionResult ContactCreated()
+        {
+            // List<Contact> ContactsCreated = Contact.GetAll();
+            return View();
+        }
         // [HttpGet("/")]
         // public ActionResult Index()
         // {
